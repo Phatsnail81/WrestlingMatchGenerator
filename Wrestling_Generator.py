2acdec83d4d9b2d7f9f464ed98b58d678b184497
Dave Bond <132367281+Phatsnail81@users.noreply.github.com>
2023-09-09 15:28:14 +0100

    WrestlingMatchGenerator
    py file

---
+++ Wrestling_Generator.py
@@ -0,0 +1,48 @@
+From = input("where's your wrestler from?: ")
+Weight = input("what does your wrestler weigh?: ")
+WrestlerName = input("what's your wrestler name?: ")
+Name = input("what's your name?: ")
+OpponentIsFrom = input("where is your opponent from?: ")
+OpponentWeight = input("what does your opponent weigh?: ")
+OpponentWrestlerName = input("what's your opponent's wrestler name?: ")
+OpponentName = input("what's your opponent's name?: ")
+WrestlerMove1 = input("first move: ")
+OpponentMove1 = input("opponent's first move: ")
+WrestlerMove2 = input("second move: ")
+OpponentMove2 = input("opponents second move: ")
+WrestlerWeapon = input("something used as a weapon: ")
+OpponentWeapon = input("what weapon your opponent will use: ")
+WrestlerInterference = input("what wrestler will interfere against you?: ")
+WrestlerFinishingMove = input("what's your finishing move?: ")
+print("==WRESTLING MATCH GENERATOR==")
+print()
+print("coming down the isle from", From)
+print("weighing", Weight)
+print(WrestlerName,Name)
+print("and his opponent, from", OpponentIsFrom)
+print("weighing", OpponentWeight)
+print(OpponentWrestlerName,OpponentName)
+print("the bell rings and", OpponentWrestlerName,"runs towards", WrestlerName, "but", WrestlerName,"moves out the way, to which", OpponentWrestlerName,
+      "crashes into the turnbuckle")
+print(WrestlerName,"turns around and get's hit with a", OpponentWeapon, "while the ref isn't looking")
+print("then", WrestlerName, "get's to his feet a bit dazed then hits a", WrestlerMove1)
+print(WrestlerName, "goes for the pin, 1 2.., and a kick out, so close there")
+print(OpponentWrestlerName, "hits a low blow, lucky the ref never seen that",OpponentWrestlerName, "hits a",OpponentMove1,"boom!")
+print(WrestlerName, "gets to his feet, eye scrape, hey what's this", WrestlerName,"pulls something out of his trunks, what is this?, it's a",
+    WrestlerWeapon,"boom!, what a hit",OpponentName, "is hurt, but look who's running down the isle, it's",WrestlerInterference,"he turns"
+      ,WrestlerName, "around, but", WrestlerName, "hit's the", WrestlerFinishingMove, "he's down, he won't get up now")
+print("he didn't see", OpponentWrestlerName,"and", WrestlerName, "gets hit with a", OpponentMove2, OpponentWrestlerName,"goes for the pin, 1 2 ..")
+print("kick out")
+print(WrestlerName, "spins", OpponentWrestlerName, "around for a", WrestlerMove2, OpponentWrestlerName,"is only dazed, here it is the", WrestlerFinishingMove)
+print(WrestlerName, "goes for the pin, 1 2 and 3, yes", WrestlerName, "wins")
+print("here is your winner", WrestlerName)
+
+
+
+
+
+
+
+
+
+
